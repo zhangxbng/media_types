16a3c371ce33a3a52875c30e197c2e68f15174d4
ZHANG XIAOBING <zhangxbng@gmail.com>
2021-05-22 17:01:31 +0800

    !A: mitools

--- src/mitools.h
+++ src/mitools.h
@@ -3,4 +3,20 @@
 namespace mitools
 {
 
+struct MediaFileInfo
+{
+	void*	cookie = nullptr;		// resevered
+
+	const char*	FileName = nullptr;
+	const char*	FileType = nullptr;
+
+	double		Duration = 0;
+
+	int			CoverLen = 0;
+	const char*	CoverPtr = nullptr;
+};
+
+extern MediaFileInfo* GetMediaFileInfo(const char* path);
+extern void FreeMediaFileInfo(MediaFileInfo*);
+
 } // namespace mitools
